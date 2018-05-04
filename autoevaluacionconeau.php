1347ddafef6a4a5d07fe099be3123d37393c4450
Santiago <sgaribotto@debian>
2018-05-04 12:01:44 -0300

    Cuadros para la CONEAU

---
+++ autoevaluacionconeau.php
@@ -0,0 +1,126 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		
+		<title>Autoevaluación CONEAU</title>
+		<?php 
+			require_once('./fuentes/meta.html');
+			
+			require 'programas.autoloader.php';
+			
+			include './fuentes/constantes.php';
+			require 'fuentes/conexion.php';
+			//$programa = new clases\Programa($_SESSION['materia'], $_SESSION['id']);
+			//$campos = $programa->mostrarCampo($ANIO, $CUATRIMESTRE);
+			$materia = $_SESSION['materia'];
+			$materia = new clases\Materia($materia);
+			$conjunto = $materia->mostrarConjunto();
+			//print_r($_SESSION);
+			//echo $materia;
+			
+			$query = "SELECT respuesta, pregunta
+						FROM autoevaluacion_coneau
+						WHERE materia IN {$conjunto}";
+			$result = $mysqli->query($query);
+			echo $mysqli->error;
+			$respuestas = array();
+			foreach (['coneau91', 'coneau92', 'coneau93', 'coneau94'] as $pregunta) {
+				$respuestas[$pregunta] = "";
+			}
+			while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+				$respuestas[$row['pregunta']] = $row['respuesta'];
+			}
+			
+		?>
+	</head>
+	
+	<body>
+		<?php
+			require_once('./fuentes/botonera.php');
+			require("./fuentes/panelNav.php");
+		?>
+		<div class="formularioLateral">
+			<div id="accordion">
+				<h3 class="formularioLateral">Analizar y evaluar la suficiencia y adecuación de los 
+					ámbitos donde se dearrolla la actividad: aulas, equipamiento didáctico, 
+					equipamiento informático, otros; y su disponibilidad para todos los alumnos.
+				</h3>
+				<div id="formulario">
+					<fieldset class="formularioLateral">
+						<form method="post" class="formularioLateral" action="procesardatos.php?formulario=autoevaluacion_coneau&pregunta=coneau91" data-pregunta='coneau91'>
+							<textarea class="formularioLateral" name="respuesta" placeholder="" style="height:150px;"><?php echo $respuestas['coneau91']; ?></textarea>
+						</form>
+						
+					</fieldset>
+				</div>
+				<h3 class="formularioLateral">Analizar los datos de la inscripción y promoción de los alumnos. 
+					Explicar los datos destacados y enunciar causas probables.
+				</h3>
+				<div id="formulario">
+					
+					<fieldset class="formularioLateral">
+						<form method="post" class="formularioLateral" action="procesardatos.php?formulario=autoevaluacion_coneau&pregunta=coneau92" data-pregunta='coneau92'>
+							<textarea class="formularioLateral" name="respuesta" placeholder="" style="height:150px;"><?php echo $respuestas['coneau92']; ?></textarea>
+						</form>
+							<img src="images/chart_example.png" />
+					</fieldset>
+				</div>
+				<h3 class="formularioLateral">Analizar y evaluar la composición del equipo docente a cargo de la actividad para llevar adelante
+					las funciones de docencia, investigación, extensión y vinculación inherentes a los cargos que han sido designados
+				</h3>
+				<div id="formulario">
+					<fieldset class="formularioLateral">
+						<form method="post" class="formularioLateral" action="procesardatos.php?formulario=autoevaluacion_coneau&pregunta=coneau93" data-pregunta='coneau93'>
+							<textarea class="formularioLateral" name="respuesta" placeholder="" style="height:150px;"><?php echo $respuestas['coneau93']; ?></textarea>
+						</form>
+						
+					</fieldset>
+				</div>
+				<h3 class="formularioLateral">Describir las acciones, reuniones, comisiones en las que participa el equipo docente para trabajar
+					sobre la articulación vertical y horizontal de los contenidos y la formación.
+				</h3>
+				<div id="formulario">
+					<fieldset class="formularioLateral">
+						<form method="post" class="formularioLateral" action="procesardatos.php?formulario=autoevaluacion_coneau&pregunta=coneau94" data-pregunta='coneau94'>
+							<textarea class="formularioLateral" name="respuesta" placeholder="" style="height:150px;"><?php echo $respuestas['coneau94']; ?></textarea>
+						</form>
+						
+					</fieldset>
+				</div>
+			</div>
+		</div>
+		
+	</body>
+	  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+	  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	  <script>
+	  $( function() {
+		$( "#accordion" ).accordion({
+			collapsible: true,
+			heightStyle: "content"
+		});
+	  } );
+	  
+	  $(document).ready(function() {
+		  $('form').submit(function(event) {
+			  event.preventDefault();
+			  var values = $(this).serialize();
+			  var pregunta = $(this).data('pregunta');
+			  $.post("procesardatos.php?formulario=autoevaluacion_coneau&pregunta=" + pregunta, 
+					values, 
+					function(data) {}
+			  );
+		  });
+		  
+	  });
+	  
+	  $('form').keyup(function() {
+		  $(this).submit();
+	  });
+	  </script>
+	  <style>
+		textarea {
+			height: 100px;
+		}
+	  </style>
+</html>
---
+++ graduadosporcohorte.php
@@ -0,0 +1,206 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		
+		<title>Graduados por cohorte</title>
+		<?php 
+			require_once('./fuentes/meta.html');
+			
+			function __autoload($class) {
+				$classPathAndFileName = "./clases/" . $class . ".class.php";
+				require_once($classPathAndFileName);
+			}
+		?>
+		
+	</head>
+	
+	<body>
+
+		<?php
+			require_once('./fuentes/botonera.php');
+			require("./fuentes/panelNav.php");
+		?>
+		<div class="formularioLateral">
+			<h2 class="formularioLateral">Graduados por cohorte</h2>
+			<!--<div id="mostrarFormulario">Mostrar Formulario</div>-->
+			<div id="mostrarFiltros">Ocultar</div>
+			
+			<div id="filtros" class="desplegable">
+				<fieldset class="formularioLateral">
+					<form method="post" class="formularioLateral filtros" action="" >
+						<label class="formularioLateral" for="anioInicio">Año Inicio:</label>
+						<input type="number" class="formularioLateral iconCod" name="anioInicio" required="required" id="anioInicio" value="2011" min="1994" max="2018" />
+						<label class="formularioLateral" for="anioFinal">Año Final:</label>
+						<input type="number" class="formularioLateral iconCod" name="anioFinal" required="required" id="anioFinal" value="2018" min="1994" max="2018" />
+						<br />
+						<label class="formularioLateral" for="carrera">Carrera:</label>
+						<select class="formularioLateral iconCod" name="carrera" required="required" id="carrera" >
+							<option value="todas">Todas</option>
+							<?php
+								require 'fuentes/conexion.php';
+								$query = "SELECT DISTINCT carrera, nombre_carrera
+											FROM analiticos
+											ORDER BY nombre_carrera";
+								//echo $query;
+								
+								$result = $mysqli->query($query);
+								
+								while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+									echo "<option value='{$row['carrera']}'>({$row['carrera']}) {$row['nombre_carrera']}</option>";
+								}	
+								
+							?>
+						</select>
+						
+						<br />
+						<button type="submit">Buscar</button>
+					</form>	
+				</fieldset>
+			</div>
+		
+			<hr>
+			
+			<div id="tablaDatos"></div>
+			
+		</div>
+		
+	</body>
+	<?php require "./fuentes/jqueryScripts.html"; ?>
+	<script src="./fuentes/funciones.js"></script>
+	
+	<script>
+		$(document).ready(function() {
+			
+			var actualizarTabla = function() {
+				formValues = $('form.filtros').serialize();
+				//console.log(formValues);
+				$('#tablaDatos').load("fuentes/AJAX.php?act=graduadosPorCohorteCONEAU", formValues, function(data) {
+					
+				});
+			}
+			
+			$('form.filtros').submit(function(event) {
+				event.preventDefault();
+				actualizarTabla();
+			});
+						
+			function togglerButtonColor() {
+				
+				var gris = '#f9f9f9';
+				
+				if ($('div#formulario').is(':visible')) {
+					$('#mostrarFormulario').css('backgroundColor', 'black');
+					$('#mostrarFormulario').css('color', gris);
+				} else {
+					$('#mostrarFormulario').css('backgroundColor', gris);
+					$('#mostrarFormulario').css('color', 'black');
+				}
+				
+				if ($('div#filtros').is(':visible')) {
+					$('#mostrarFiltros').css('backgroundColor', 'black');
+					$('#mostrarFiltros').css('color', gris);
+				} else {
+					$('#mostrarFiltros').css('backgroundColor', gris);
+					$('#mostrarFiltros').css('color', 'black');
+				}
+			}
+			
+			
+			$('#mostrarFormulario').click(function() {
+				$('div #formulario').slideToggle(function() {
+					if ($('div#formulario').is(':visible')) {
+					$('#mostrarFormulario').css('backgroundColor', 'black');
+					$('#mostrarFormulario').css('color', gris);
+				} else {
+					$('#mostrarFormulario').css('backgroundColor', gris);
+					$('#mostrarFormulario').css('color', 'black');
+				}
+					
+				});
+				$('div #filtros').slideUp();
+				
+				var gris = '#f9f9f9';
+				$('#mostrarFiltros').css('backgroundColor', gris);
+					$('#mostrarFiltros').css('color', 'black');
+				
+				
+			});
+			
+			$('#mostrarFiltros').click(function() {
+				$('div #filtros').slideToggle(function() {
+					if ($('div#filtros').is(':visible')) {
+						$('#mostrarFiltros').css('backgroundColor', 'black');
+						$('#mostrarFiltros').css('color', gris);
+					} else {
+						$('#mostrarFiltros').css('backgroundColor', gris);
+						$('#mostrarFiltros').css('color', 'black');
+					}
+				});
+				$('div #formulario').slideUp();
+				
+				var gris = '#f9f9f9';
+				
+				$('#mostrarFormulario').css('backgroundColor', gris);
+				$('#mostrarFormulario').css('color', 'black');
+				
+				
+			});
+			$('#mostrarFiltros').click();
+			
+			/*$('#filtro').on('keyup', function(event) {
+				if ($(this).val().length > 1) {
+					actualizarTabla();
+				}
+			});
+			
+			$('#filtro').focus();*/
+			
+			$.ajaxSetup({
+				contentType: "application/x-www-form-urlencoded;charset=UTF-8"
+			});
+			var dialogOptions = {
+				autoOpen: false,
+				width:1000,
+				height: 600,
+				modal: true,
+				appendTo: "#Botonera",
+				close: function() {
+					$('#mostrarFormulario').off('click');
+					$('#mostrarFormulario').click(function() {
+						$('div #formulario').slideToggle();
+					
+					});
+				},
+					
+			};
+			
+			/*$(".datepicker").datepicker({
+				changeMonth:true,
+				changeYear:true,
+				dateFormat:'yy-mm-dd',
+				yearRange:'c-80:c',
+				
+			});*/
+			
+			$('select').combobox();
+		});
+	</script>
+	<style>
+	  .custom-combobox {
+		position: relative;
+		display: inline-block;
+	  }
+	  .custom-combobox-toggle {
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		margin-left: -1px;
+		padding: 0;
+	  }
+	  .custom-combobox-input {
+		margin: 0;
+		padding: 5px 10px;
+		width:300px;
+	  }
+  </style>
+</html>
